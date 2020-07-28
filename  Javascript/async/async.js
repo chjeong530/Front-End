7a71a4316b835fd2696e9c74dd2c9975216021c6
Chanhee <chjeong530@gmail.com>
2020-07-28 22:21:38 +0900

    async

---
+++  Javascript/async/async.js
@@ -0,0 +1,12 @@
+// async & await
+// clear style of using promiss :)
+
+// 1. async
+function fetchUser() {
+  // do network request in 10 secs....
+
+  return 'chan';
+}
+
+const user = fetchUser();
+console.log(user);
