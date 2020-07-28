99d56acaa20e398165ea0ccc4a1e8253ae189869
Chanhee <chjeong530@gmail.com>
2020-07-28 22:14:21 +0900

    callback to promiss

---  Javascript/async/callback-to-promiss.js
+++  Javascript/async/callback-to-promiss.js
@@ -1,70 +1,65 @@
 'use strict';
 
-// Synchronous callback
-function printImmediately(print) {
-  print();
-}
-
-// JavaScript is synchronous.
-// Execute the code block in order after hoisting.
-// hoisting: var, function declaration
-console.log('1');
-setTimeout( () => console.log('2'), 1000);
-console.log('3');
-
-printImmediately(() => console.log('hello'));
-
-
-// Asynchronous callback
-function printWithDelay(print, timeout) {
-  setTimeout(print, timeout);
-}
-printWithDelay( () => console.log('async callback'), 2000);
-
-
 // Callback Hell example
+// class UserStorage{
+//   loginUser(id, password, onSuccess, onError) {
+//     setTimeout( () => {
+//       if (
+//         (id === 'chan' && password === ' dream') ||
+//         (id === 'coder' && password === ' academy')
+//       ) {
+//         onSuccess(id);
+//       } else {
+//         onError(new Error('not found'));
+//       }
+//     }, 2000);
+//   }
+
+//   getRoles(user, onSuccess, onError) {
+//     setTimeout( () => {
+//       if (id === 'chan'){
+//         onSuccess({ name:'chan', role:'admin'});
+//       } else {
+//         onError(new Error('no access'));
+//       }
+//     }, 1000);
+//   }
+// }
+
 class UserStorage{
-  loginUser(id, password, onSuccess, onError) {
-    setTimeout( () => {
-      if (
-        (id === 'chan' && password === ' dream') ||
-        (id === 'coder' && password === ' academy')
-      ) {
-        onSuccess(id);
-      } else {
-        onError(new Error('not found'));
-      }
-    }, 2000);
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout( () => {
+        if (
+          (id === 'chan' && password === ' dream') ||
+          (id === 'coder' && password === ' academy')
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error('not found'));
+        }
+      }, 2000);
+    });
   }
 
-  getRoles(user, onSuccess, onError) {
-    setTimeout( () => {
-      if (id === 'chan'){
-        onSuccess({ name:'chan', role:'admin'});
-      } else {
-        onError(new Error('no access'));
-      }
-    }, 1000);
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout( () => {
+        if (id === 'chan'){
+          resolve({ name:'chan', role:'admin'});
+        } else {
+          reject(new Error('no access'));
+        }
+      }, 1000);
+    });
   }
 }
-
 
 const userStorage = new UserStorage();
 const id = prompt('enter your id');
 const password = prompt('enter your password');
-userStorage.loginUser(
-  id, 
-  password, 
-  (user) => {
-    userStorage.getRoles(
-      user, 
-      userWithRole =>  {
-        alert(`Hello ${userWithRole.name}, you have a ${userWithRole.role} role`);
-      },
-      error => {}
-    );
-  },
-  error => {
-    console.log(error)
-  }
-);
+userStorage
+  .loginUser(id, password)
+  .then(userStorage.getRoles)
+  .then(user => alert(`Hello ${user.name}, you have a ${user.role} role`))
+  .catch(console.log);
